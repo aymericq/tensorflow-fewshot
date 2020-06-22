2dec71098e366a47e2e35bee173be405f69f921d
Aymeric Quesne <aymeric.quesne@octo.com>
2020-06-22 09:43:25 +0200

    Add python environment managing facility

---
+++ setup_env.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+readonly SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd);
+readonly ENV_NAME=fsl
+
+function main
+{
+    echo "[*] Install fsl conda environment.";
+
+    set -o errexit
+    set -o pipefail
+    set -o nounset
+    set -o errtrace
+
+    install_env;
+}
+
+function install_env
+{
+    echo "[*] Installing dependencies...";
+    if conda env list | grep -q ${ENV_NAME}
+    then
+        echo "[*] ${ENV_NAME} env found, updating...";
+        conda env update -q -f ${SCRIPT_DIRECTORY}/environment.yml -n ${ENV_NAME};
+    else
+        echo "[*] Creating ${ENV_NAME} env...";
+        conda env create -q -f ${SCRIPT_DIRECTORY}/environment.yml -n ${ENV_NAME};
+    fi
+
+    echo "[*] Activating ${ENV_NAME}..";
+    PS1=${PS1:-}
+    source activate ${ENV_NAME};
+
+    echo "[*] Done !";
+}
+
+main

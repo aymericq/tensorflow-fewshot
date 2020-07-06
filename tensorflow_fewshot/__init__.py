0089a31d67675a57909ca35b5fd90e8330184ca3
Aymeric Quesne <aymeric.quesne@octo.com>
2020-07-06 12:08:38 +0200

    Add init to package.

--- tensorflow_fewshot/__init__.py
+++ tensorflow_fewshot/__init__.py
@@ -1,4 +1,4 @@
-""" Few-shot Classification is a Tensorflow-based library to train, evaluate and make predictions using few shot models.
+"""Tensorflow-Fewshot is a Tensorflow-based library to train, evaluate and make predictions using few shot models.
 
-FSC is intended to make the training of popular models convenient and as little verbose as possible.
+TF-FS is intended to make the training of popular models convenient and as little verbose as possible.
 """

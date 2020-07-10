8976cc7e578e72f8279f6c2b34b19df496f4396d
Aymeric Quesne <aymeric.quesne@octo.com>
2020-07-10 10:57:19 +0200

    Refactor methods names and documentation.

--- tensorflow_fewshot/models/prototypical_network.py
+++ tensorflow_fewshot/models/prototypical_network.py
@@ -3,7 +3,11 @@ import numpy as np
 
 
 class PrototypicalNetwork:
-    """Implements Prototypical Networks from the original paper (Snell et al., 2017)."""
+    """Implements Prototypical Networks from the original paper (Snell et al., 2017).
+
+    Args:
+        encoder (tf.keras.Model): The encoder used to map the image onto the metric space.
+    """
 
     def __init__(
             self,
@@ -32,12 +36,31 @@ class PrototypicalNetwork:
             optimizer='Adam',
             logging_interval=200
     ):
-        """Trains the model on the meta-training set."""
+        """Trains the model on the meta-training set.
+
+        Args:
+            meta_train_X (numpy.array): The data set used for meta-training, must be of dimension 4.
+            meta_train_Y (numpy.array): The corresponding set of labels, must be a single dimension array of integers
+            meta_val_X (numpy.array): A validation set to track the performance of the model every few episodes.
+            meta_val_Y (numpy.array): The corresponding set of validation labels.
+            n_episode (int): Number of episodes for meta-training.
+            n_way (int): Number of ways (or classes per episode).
+            ks_shots (int): Number of image per class in the support set.
+            kq_shots (int): Number of image per class in the query set.
+            optimizer (tf.keras.optimizer): A valid Keras optimizer for training.
+            logging_interval (int): log and display metrics every ‘logging_interval‘ episodes.
+
+        Return:
+            3-tuple:
+                - acc (list(float)): history of accuracy logged every ‘logging_interval‘ episodes.
+                - loss (list(float)): history of loss logged every ‘logging_interval‘ episodes.
+                - test_acc (list(float)): history of vaidation accuracy logged every ‘logging_interval‘ episodes.
+        """
         acc = []
         loss = []
         test_acc = []
 
-        lr_schedule, optimizer = self.compile_model(n_episode, optimizer)
+        lr_schedule, optimizer = self._prepare_optimizer(n_episode, optimizer)
 
         for episode in range(n_episode):
             # Open a GradientTape to record the operations run
@@ -82,10 +105,10 @@ class PrototypicalNetwork:
                 )
         return acc, loss, test_acc
 
-    def compile_model(self, n_episode, optimizer):
-        """Compile the model.
+    def _prepare_optimizer(self, n_episode, optimizer):
+        """Compiles the model.
 
-        Build the optimizer and the learning schedule according to the number of episodes, then compiles the model.
+        Builds the optimizer and the learning schedule according to the number of episodes, then compiles the model.
         """
         if optimizer == 'Adam':
             if n_episode > 2000:
@@ -102,8 +125,16 @@ class PrototypicalNetwork:
         self.encoder.compile(optimizer)
         return lr_schedule, optimizer
 
-    def train(self, train_X, train_Y):
-        """Train the model."""
+    def fit(self, train_X, train_Y):
+        """Fits the model to the data.
+
+        Computes the prototype of each class and the internal mapping between prototype index
+        and label.
+
+        Args:
+            train_X (numpy.array): A 4-dimensional array.
+            train_Y (numpy.array): The corresponding labels, a 1-dimensional array of integers.
+        """
         n_labels = len(np.unique(train_Y))
         prototypes = np.zeros((n_labels, self.output_dim)).astype(np.float32)
 
@@ -124,12 +155,19 @@ class PrototypicalNetwork:
         self.prototypes = prototypes
 
     def predict(self, X):
-        """Make predictions and return the inferred labels."""
+        """Make predictions and return the inferred labels.
+
+        Args:
+            X (numpy.array): A 4-dimensional array.
+
+        Returns:
+            preds (numpy.array): The predicted label for each data point, as a 1-dimensional array of integers.
+        """
         dists = _euclidean_distance(self.prototypes, self.encoder(X).numpy())
         return self._proto_index_to_label[tf.argmin(dists, axis=0).numpy()]
 
 
-def _create_imageNetCNN(nb_hidden_layers=4, nb_filters=64, output_dim=64):
+def _create_imageNetCNN(nb_hidden_layers=4, nb_filters=64):
     """Creates a Keras Sequential Model as described in Matching Nets paper (Vinyals et al., 2016)."""
 
     layers = []
@@ -144,30 +182,42 @@ def _create_imageNetCNN(nb_hidden_layers=4, nb_filters=64, output_dim=64):
     return tf.keras.models.Sequential(layers)
 
 
-def _run_episode(train_set_X, train_set_y, n_way, ks_shots, kq_shots, encoder):
-    """
+def _run_episode(episode_X, episode_y, n_way, ks_shots, kq_shots, encoder):
+    """ Computes softmax of distances for one sampled episode.
+
     Given a set X of input images, their corresponding labels y, episode
     parameters, N-way, K-support shots, K-query shots and an encoder:
     Sample a support set and a query set according to the episode's parameters
     then computes the prototypes from the support set, and return for each image
     of the query set, the negative logsoftmax of the the (negative) distance to
     each prototype.
+
+    Args:
+        episode_X (numpy.array): The dataset to sample images from.
+        episode_y (numpy.array): The corresponding labels.
+        n_way (int): Number of ways (or classes per episode).
+        ks_shots (int): Number of image per class in the support set.
+        kq_shots (int): Number of image per class in the query set.
+        encoder (tf.keras.Model): The encoder used to map the image onto the metric space.
+
+    Returns:
+        dist (numpy.array): the distance of each query datapoint to each prototype.
     """
-    im_width, im_height, im_n_channels = train_set_X.shape[-3:]
+    im_width, im_height, im_n_channels = episode_X.shape[-3:]
 
     # Sample N-way, KS support shots, KS query shots
-    classes = np.random.choice(np.unique(train_set_y), n_way)
+    classes = np.random.choice(np.unique(episode_y), n_way)
     support_indices = np.zeros((n_way, ks_shots)).astype(np.int32)
     query_indices = np.zeros((n_way, kq_shots)).astype(np.int32)
 
     for i in range(n_way):
-        indices = np.random.choice(np.argwhere(train_set_y == classes[i]).flatten(), ks_shots + kq_shots)
+        indices = np.random.choice(np.argwhere(episode_y == classes[i]).flatten(), ks_shots + kq_shots)
         support_indices[i, :] = indices[:ks_shots]
         query_indices[i, :] = indices[ks_shots:]
 
     # Forward support into encoder
     support_embeddings = tf.reshape(
-        encoder(train_set_X[tf.reshape(support_indices, (n_way * ks_shots,)), :, :, :]),
+        encoder(episode_X[tf.reshape(support_indices, (n_way * ks_shots,)), :, :, :]),
         (n_way, ks_shots, -1)
     )
 
@@ -176,7 +226,7 @@ def _run_episode(train_set_X, train_set_y, n_way, ks_shots, kq_shots, encoder):
 
     # Forward pass on query set
     query_embeddings = tf.reshape(
-        encoder(train_set_X[tf.reshape(query_indices, (n_way * kq_shots,)), :, :, :]),
+        encoder(episode_X[tf.reshape(query_indices, (n_way * kq_shots,)), :, :, :]),
         (n_way * kq_shots, -1)
     )
 
--- tests/models/test_prototypical_networks.py
+++ tests/models/test_prototypical_networks.py
@@ -1,6 +1,7 @@
 import tensorflow_fewshot.models.prototypical_network as pn
 from numpy.random import normal
 
+
 def test_l2_norm_shape_is_10_5_when_passed_10_prototypes_10_embeddings():
     # Given
     prototypes = normal(size=(5, 64))
@@ -12,10 +13,11 @@ def test_l2_norm_shape_is_10_5_when_passed_10_prototypes_10_embeddings():
     # Then
     assert distances.shape == (5, 10)
 
+
 def test_encoder_output_shape_is_10_64_when_passed_10_images():
     # Given
     encoder = pn._create_imageNetCNN()
-    images = normal(size=(10,28,28,1))
+    images = normal(size=(10, 28, 28, 1))
 
     # When
     embeddings = encoder(images)

d53773b30f3acaa780904b7d99c83d3c030bf16a
Aymeric Quesne <aymeric.quesne@octo.com>
2020-07-16 12:16:56 +0200

    Add test for euclidean distance correctness.

--- tests/models/test_prototypical_networks.py
+++ tests/models/test_prototypical_networks.py
@@ -15,6 +15,29 @@ def test_l2_norm_shape_is_10_5_when_passed_10_prototypes_10_embeddings():
     # Then
     assert distances.shape == (5, 10)
 
+def test_euclidean_distance_is_right_on_two_examples():
+    # Given
+    prototypes = np.ones((2, 64))
+    prototypes[0, :] *= 1
+    prototypes[1, :] *= 4
+    query_embeddings = np.ones((2, 64))
+    query_embeddings[0, :] *= 3
+    query_embeddings[1, :] *= 5
+
+    true_dists = np.zeros((2,2))
+    true_dists[0, 0] = 8*2 # sqrt(64 * (1-3)^2)
+    true_dists[1, 0] = 8   # sqrt(64 * (4-3)^2)
+    true_dists[0, 1] = 8*4 # sqrt(64 * (1-5)^2)
+    true_dists[1, 1] = 8   # sqrt(64 * (4-5)^2)
+
+
+    # When
+    distances = pn._euclidean_distance(prototypes, query_embeddings)
+
+    # Then
+    assert distances.shape == (2, 2)
+    assert np.all(distances == true_dists)
+
 
 def test_encoder_output_shape_is_10_64_when_passed_10_images():
     # Given

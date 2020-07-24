27f43f58b7ec378bb152b891f61b3a2e20fc758f
Aymeric Quesne <aymeric.quesne@octo.com>
2020-07-24 11:57:55 +0200

    Add custom loss capability to fit.

--- tensorflow_fewshot/models/maml.py
+++ tensorflow_fewshot/models/maml.py
@@ -12,12 +12,15 @@ def apply_one_gradient_step(model, grads, alpha):
 class MAML:
     """Implements Model-Agnostic Meta-Learning (Finn et al., 2017)."""
 
-    def __init__(self, model=None):
+    def __init__(self, model, loss):
         self.model = model
+        self.loss = loss
 
     def fit(self, data_x, data_y, alpha=1):
         with tf.GradientTape() as tape:
             preds = self.model(data_x)
+            assert preds.shape == data_y.shape
+            loss_value = self.loss(data_y, preds)
 
-        grads = tape.gradient(preds, self.model.weights)
+        grads = tape.gradient(loss_value, self.model.weights)
         apply_one_gradient_step(self.model, grads, alpha)
--- tests/models/test_maml.py
+++ tests/models/test_maml.py
@@ -3,18 +3,7 @@ import tensorflow as tf
 import numpy as np
 
 
-def test_instantiate_MAML():
-    # Given
-
-    # When
-    maml = MAML()
-
-    # Then
-    assert maml is not None
-
-
-def test_fit_on_two_layer_perceptron_is_correct():
-    # Given
+def create_2l_perceptron():
     model = tf.keras.models.Sequential([
         tf.keras.layers.Input((1,)),
         tf.keras.layers.Dense(2, use_bias=False),
@@ -26,14 +15,79 @@ def test_fit_on_two_layer_perceptron_is_correct():
     ]
     model.set_weights(weights)
 
-    maml = MAML(model)
+    return model
+
+
+def test_instantiate_MAML():
+    # Given
+    model = tf.keras.models.Sequential()
+    loss = tf.keras.losses.MSE
+
+    # When
+    maml = MAML(model, loss)
+
+    # Then
+    assert maml is not None
+
+
+def test_fit_on_two_layer_perceptron_is_correct():
+    # Given
+    model = create_2l_perceptron()
+
+    maml = MAML(model, loss=lambda y, p: p)
 
     data_x = np.ones((1,))
-    data_y = np.zeros((1,))
+    data_y = np.zeros((1, 1))
 
     expected_weights = [
         np.zeros((1, 2)),
         np.zeros((2, 1)),
+    ]
+
+    # When
+    maml.fit(data_x, data_y)
+    weight_set = maml.model.get_weights()
+
+    # Then
+    for i_weight, weights in enumerate(weight_set):
+        assert np.all(weights == expected_weights[i_weight])
+
+
+def test_fit_on_two_layer_perceptron_is_correct_when_passed_a_value_for_alpha():
+    # Given
+    model = create_2l_perceptron()
+
+    maml = MAML(model, loss=lambda y, p: p)
+
+    data_x = np.ones((1,))
+    data_y = np.zeros((1, 1))
+
+    expected_weights = [
+        0.5 * np.ones((1, 2)),
+        0.5 * np.ones((2, 1)),
+    ]
+
+    # When
+    maml.fit(data_x, data_y, alpha=0.5)
+    weight_set = maml.model.get_weights()
+
+    # Then
+    for i_weight, weights in enumerate(weight_set):
+        assert np.all(weights == expected_weights[i_weight])
+
+
+def test_fit_on_two_layer_perceptron_is_correct_when_used_with_a_loss():
+    # Given
+    model = create_2l_perceptron()
+
+    maml = MAML(model, loss=tf.keras.losses.MSE)
+
+    data_x = np.ones((1,))
+    data_y = np.zeros((1, 1))
+
+    expected_weights = [
+        -3*np.ones((1, 2)),
+        -3*np.ones((2, 1)),
     ]
 
     # When

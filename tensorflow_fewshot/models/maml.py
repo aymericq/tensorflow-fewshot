4707c2cfed179a605ebf2febc127711cef262bcc
Aymeric Quesne <aymeric.quesne@octo.com>
2020-07-24 10:48:54 +0200

    Create MAML class and add fit method.

---
+++ tensorflow_fewshot/models/maml.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+
+def apply_one_gradient_step(model, grads, alpha):
+    curr_weights = model.get_weights()
+    new_weights = []
+    for i in range(len(curr_weights)):
+        new_weights.append(curr_weights[i] - alpha*grads[i])
+    model.set_weights(new_weights)
+
+
+class MAML:
+    """Implements Model-Agnostic Meta-Learning (Finn et al., 2017)."""
+
+    def __init__(self, model=None):
+        self.model = model
+
+    def fit(self, data_x, data_y, alpha=1):
+        with tf.GradientTape() as tape:
+            preds = self.model(data_x)
+
+        grads = tape.gradient(preds, self.model.weights)
+        apply_one_gradient_step(self.model, grads, alpha)
---
+++ tests/models/test_maml.py
@@ -0,0 +1,45 @@
+from tensorflow_fewshot.models.maml import MAML
+import tensorflow as tf
+import numpy as np
+
+
+def test_instantiate_MAML():
+    # Given
+
+    # When
+    maml = MAML()
+
+    # Then
+    assert maml is not None
+
+
+def test_fit_on_two_layer_perceptron_is_correct():
+    # Given
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Input((1,)),
+        tf.keras.layers.Dense(2, use_bias=False),
+        tf.keras.layers.Dense(1, use_bias=False),
+    ])
+    weights = [
+        np.ones((1, 2)),
+        np.ones((2, 1)),
+    ]
+    model.set_weights(weights)
+
+    maml = MAML(model)
+
+    data_x = np.ones((1,))
+    data_y = np.zeros((1,))
+
+    expected_weights = [
+        np.zeros((1, 2)),
+        np.zeros((2, 1)),
+    ]
+
+    # When
+    maml.fit(data_x, data_y)
+    weight_set = maml.model.get_weights()
+
+    # Then
+    for i_weight, weights in enumerate(weight_set):
+        assert np.all(weights == expected_weights[i_weight])

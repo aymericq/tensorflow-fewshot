bf838e4b277962a9b441f31f9be24c2e77bc1e63
Aymeric Quesne <aymeric.quesne@octo.com>
2020-08-03 18:57:12 +0200

    Add doc.

--- tensorflow_fewshot/datasets/meta_dataset.py
+++ tensorflow_fewshot/datasets/meta_dataset.py
@@ -4,6 +4,29 @@ from matplotlib.pyplot import imread
 class MetaDataset:
 
     def __init__(self, path):
+        """Creates a meta-dataset from a file path.
+
+        Creates a meta-dataset given of tree of folders of the following form:
+        dataset/
+            meta/
+                label_a/
+                    img_001.jpg
+                    img_002.jpg
+                    ...
+                label_b/
+                    img_257.jpg
+                    ...
+            train/
+                label_1/
+                    train_1.jpg
+                    ...
+                label_2/
+                    train_6.jpg
+                    ...
+
+        Args:
+             path (string): The path of the root folder of the dataset.
+        """
         self.path = path
         self.meta_path = '/'.join((self.path, 'meta'))
         self.train_path = '/'.join((self.path, 'train'))
@@ -17,6 +40,11 @@ class MetaDataset:
             self.train_labels[label] = [elem for elem in listdir("/".join((self.train_path, label)))]
 
     def get_meta_dataset_generator(self):
+        """Returns a generator of the whole meta-dataset.
+
+        Returns:
+            generator: a generator of tuples (image, label).
+        """
         def generator_factory(mds):
             for label in mds.meta_labels:
                 for im_path in mds.meta_labels[label]:
@@ -28,6 +56,11 @@ class MetaDataset:
         return generator_factory(self)
 
     def get_train_dataset_generator(self):
+        """Returns a generator of the whole training dataset.
+
+        Returns:
+            generator: a generator of tuples (image, label).
+        """
         def generator_factory(mds):
             for label in mds.train_labels:
                 for im_path in mds.train_labels[label]:
